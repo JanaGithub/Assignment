b5765cc34a6a3a5ff740b5be1574adc74d6d7226
sarthak jana <sarthak jana>
2017-01-14 18:30:41 +0530

    module 4

---
+++ mod_4_solution/SpeakGoodbye.js
@@ -0,0 +1,9 @@
+(function(){
+	var byespeaker = {};
+	var speakword = "Good Bye";
+	byespeaker.speak = function (name){
+		console.log(speakword + " " + name);
+	}
+
+	window.byespeaker = byespeaker;
+})();
